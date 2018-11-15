fa8bb6612591d9c2a2657e205f17c6fb8791208f
Kevin W <wongkokwah@gmail.com>
2018-11-15 22:10:40 +0800

    init commit

---
+++ src/components/navbar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const NavBar = () => {
+  return (
+    <AppBar
+      title="Pixabay Image Finder"
+    
+    >
+    
+    </AppBar>
+  )
+}
+
+export default NavBar;
